1716e0a5c7a1e1af4c67fb1a9525cc93be8b283b
Robert Durfee <robert.durfee.17@gmail.com>
2018-12-11 11:22:44 -0500

    Created scripts for adding flights radar SQL table

---
+++ Radar/CreateRadarTable.sql
@@ -0,0 +1,11 @@
+USE `FlightsDatabase`;
+
+CREATE TABLE `Radar` (
+  `RadarID` int NOT NULL AUTO_INCREMENT,
+  `CreatedDateTime` DATETIME(6),
+  `State` VARCHAR(50),
+  `URL` VARCHAR(100),
+  PRIMARY KEY (`RadarID`)
+);
+
+CREATE UNIQUE INDEX `UQ_Radar_CreatedDateTime_State` ON `Radar`(`CreatedDateTime`, `State`);
