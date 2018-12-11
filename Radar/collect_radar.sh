90d5722fcf7cb31ed548eac3ff171aeabd8dc56a
Robert Durfee <robert.durfee.17@gmail.com>
2018-12-11 15:18:18 +0000

    Updated script to log status

--- Radar/collect_radar.sh
+++ Radar/collect_radar.sh
@@ -2,4 +2,9 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS="/home/rober/Flights/flights-service-account-key.json"
 
-python3 /home/rober/Flights/Radar/collect_radar.py
+date=$(date +%Y%m%dT%H%M%S)
+printf $date
+
+/usr/bin/python3.5 /home/rober/Flights/Radar/collect_radar.py || { printf " Failed\n" ; exit 1; }
+
+printf " Success\n"
