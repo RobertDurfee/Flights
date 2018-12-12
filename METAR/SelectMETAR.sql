d3e76db8b3fb55476cf9daf682bf8aa44feb72f0
Robert Durfee <robert.durfee.17@gmail.com>
2018-12-11 22:41:43 -0500

    Drafted select query

---
+++ METAR/SelectMETAR.sql
@@ -0,0 +1,16 @@
+USE `FlightsDatabase`;
+
+SELECT *
+FROM `METAR` as mt
+LEFT JOIN `RunwayVisibility` AS rv
+  ON mt.`METARID` = rv.`METARID`
+LEFT JOIN `PresentWeather` AS pw
+  ON mt.`METARID` = pw.`METARID`
+LEFT JOIN `RecentWeather` AS rw
+  ON mt.`METARID` = rw.`METARID`
+LEFT JOIN `SkyCondition` AS sc
+  ON mt.`METARID` = sc.`METARID`
+LEFT JOIN `WindShearRunway` AS ws
+  ON mt.`METARID` = ws.`METARID`
+LEFT JOIN `Remark` AS rm
+  ON mt.`METARID` = rm.`METARID`;
