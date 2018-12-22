e608ba9f1a8df719687cadb43d320df428f798e5
Robert Durfee <robert.durfee.17@gmail.com>
2018-12-22 11:47:50 -0600

    Added schedule select query

---
+++ Schedule/SelectSchedule.sql
@@ -0,0 +1,17 @@
+USE `Schedule`;
+
+SELECT `Flight`.`ICAONumber` AS `FlightNumber`,
+       `Schedule`.`Status` AS `Status`,
+       `Departure`.`ICAOCode` AS `Departure`,
+       `Arrival`.`ICAOCode` AS `Arrival`,
+       `Departure`.`ScheduledTime` AS `ScheduledDeparture`,
+       `Arrival`.`ScheduledTime` AS `ScheduledArrival`,
+       `Departure`.`AcutalTime` AS `ActualDeparture`,
+       `Arrival`.`ActualTime` AS `ActualArrival`
+FROM `Schedule`
+INNER JOIN `Flight`
+  ON `Schedule`.`FlightID` = `Flight`.`FlightID`
+INNER JOIN `Departure`
+  ON `Schedule`.`ScheduleID` = `Departure`.`ScheduleID`
+INNER JOIN `Arrival`
+  ON `Schedule`.`ScheduleID` = `Arrival`.`ScheduleID`
